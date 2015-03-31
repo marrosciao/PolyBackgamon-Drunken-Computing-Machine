f31800f92008a25d737898f4713d607d6ba0c304
Cobrand <cobrandw@gmail.com>
2015-03-31 13:43:37 +0200

    Changement structure AI_tree *

--- strategy/include/AI_tree.h
+++ strategy/include/AI_tree.h
@@ -1,9 +1,14 @@
 #include "backgammon.h"
 
 
+
 typedef struct AINode AINode ;
 // pour un noeud donné, stocke la liste de tous les eventuels sous noeuds possible
 // stocke un etat de board donné
 // stocke un entier, la "valeur" du board, plus elle est élevée, plus le joueur est dans un etat avantageux
 
-typedef struct AIListNodes AIListNodes ;
+typedef struct AIListPossibilities AIListPossibilities ;
+
+int getValueFromGameState(SGameState etat_jeu, Player player);
+AIListPossibilities retrieveEveryPossibility(SGameState game, Player player, const unsigned char dices[2] );
+void deleteTree(AINode node);
--- strategy/src/AI_tree.c
+++ strategy/src/AI_tree.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 // size_t
 
-struct AIListNodes
+struct AIListPossibilities
 {
-    AINode* liste ;
+    AINode* liste_noeuds ;
     //! tableau des noeuds
+    
+    SMove* liste_mouvements ;
+    //! tableau des mouvements
+    // Pour aller à la position liste_noeuds[i], il faut faire le mouvement liste_mouvements[i]
+    // Par conséquence liste_mouvements a la même taille que liste_noeuds
+    
     
     size_t taille ;
     //! taille du tableau des noeuds
@@ -17,7 +23,7 @@ struct AINode
     SGameState etat_jeu ; 
     //! etat du jeu pour un noeud donne
     
-    AIListNodes liste_noeuds ;
+    AIListPossibilities liste_possibilites ;
     //! liste des branches (ou noeuds)
     
     int valeur ;
@@ -32,10 +38,10 @@ int getValueFromGameState(SGameState etat_jeu, Player player)
 }
 
 
-AIListNodes retrieveEveryPossibility(SGameState game, Player player, const unsigned char dices[2] )
+AIListPossibilities retrieveEveryPossibility(SGameState game, Player player, const unsigned char dices[2] )
 {
     // renvoie une liste de toutes les possibilités à partir d'un GameState
-
+    // liste_noeuds doit être egal à NULL dans la structure renvoyée
 }
 
 
@@ -45,13 +51,16 @@ void deleteTree(AINode node)
 {
     // efface proprement un arbre
     
-    if (node.liste_noeuds.liste){
-        for (size_t i = 0 ; i < node.liste_noeuds.taille ; i++)
+    if (node.liste_possibilites.liste_noeuds){
+        for (size_t i = 0 ; i < node.liste_possibilites.taille ; i++)
         {
-            deleteTree(node.liste_noeuds.liste[i]);
+            deleteTree(node.liste_possibilites.liste_noeuds[i]);
+            
         }
-        free(node.liste_noeuds.liste);
+        free(node.liste_possibilites.liste_noeuds);
     }
+    if (node.liste_possibilites.liste_mouvements)
+                free(node.liste_possibilites.liste_mouvements);
 }
 
 
