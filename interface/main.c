ddf8d846b46b846cb5449826b2bbc22c6263ee28
n3f4s <tiv.crb@gmail.com>
2015-03-31 14:00:30 +0200

    MAJ des CMakeLists et debug et ameliotation main
    CMakeLists.txt -> debug CMakeLists.txt de la racine
    main -> meilleur gestion des paths

--- interface/main.c
+++ interface/main.c
@@ -1,11 +1,14 @@
 #include<stdbool.h>
 #include<assert.h>
 #include<dlfcn.h>
+#include<string.h>
+#include<stdlib.h>
 
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
 
-#include"include/backgammon.h"
+#include"backgammon.h"
+#include"config.h"
 
 
 typedef struct{
@@ -45,16 +48,20 @@ int main(){
 	// --- Initialisation des bibliothèques
 	void* white_handle = NULL;
 	Functions white_funcs;
-	init_lib("../strategy/libstrategy.so", &white_handle, &white_funcs);
+	char* white_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
+	sprintf(white_path, "%s/libstrategy.so", LIB_PATH);
+	init_lib(white_path, &white_handle, &white_funcs);
 	char white_name[50];
-	white_funcs.initLibrary(white_name);//TODO : erreur lors de la recopie du nom
+	white_funcs.initLibrary(white_name);
 	printf("White I.A. : %s\n", white_name);
 
 	void* black_handle = NULL;
 	Functions black_funcs;
-	init_lib("../strategy/libstrategy.so", &black_handle, &black_funcs);
+	char* black_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
+	sprintf(black_path, "%s/libstrategy.so", LIB_PATH);
+	init_lib(black_path, &black_handle, &black_funcs);
 	char black_name[50];
-	black_funcs.initLibrary(black_name);//TODO : erreur lors de la recopie du nom
+	black_funcs.initLibrary(black_name);
 	printf("Black I.A. : %s\n", black_name);
 
 	// --- Fermeture des bibliothèques
