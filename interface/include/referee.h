ef64e1c83903aa9eef2240da73206eba628074a9
n3f4s <tiv.crb@gmail.com>
2015-04-01 17:28:10 +0200

    ajout doc et MAJ/refactoring referee et main

--- interface/include/referee.h
+++ interface/include/referee.h
@@ -3,20 +3,102 @@
 
 #include"backgammon.h"
 
+/**
+ * @file referee.h
+ * @author Vincent BONNEVALLE
+ * @brief Définie les fonctions nécessaires au déplacement des pions
+ * @todo Vérifier qu'on utilise le max de dés possible (on doit utiliser le max de dé)
+ */
+
+//TODO : faire les test (cf CMakeLists.txt)
+//TODO : revoir les prototypes pour enlever les paramêtre inutiles
+//TODO : revoir les pré/post pour faire plus proche de spec
+
+//! typedef définissant le type d'un dé
 typedef const unsigned char Dice;
+//! raccourci pour const unsigned int
 typedef const unsigned int cuint;
+//! raccourci pour unsigned int
 typedef unsigned int uint;
 
-//! return 0 if the movement are correct, 1 else
+/**
+ * @fn int check_move(const SMove movement, Dice[] dices, cuint nbDice, const Player player, SGameState * const state)
+ * @brief Vérifie la validité du mouvement
+ * @param movement mouvement dont on veut vérifier la validité
+ * @param dices    valeur du lancé de dés
+ * @param nbDice   nombre de dés lancé
+ * @param player   joueur dont on veut vérifier les mouvements
+ * @param state    état du jeux
+ * @return 0 -> pas d'erreur, 1 -> le mouvement est invalide
+ */
 int check_move(const SMove, Dice[], cuint, const Player, SGameState * const);
 
-//! return the number of bad moves
-int move_all(SGameState * const state, SMove const * const moves,
-		cuint nb_moves, Dice dices[], cuint nb_dices, const Player player);
+/**
+ * @fn int move_all(SGameState * const state, SMove const * const moves, cuint nb_moves, Dice dices[], cuint nb_dices, const Player player)
+ * @brief Déplace les pions selon les mouvements (valide) passé en paramêtre
+ * @param state   état du jeux
+ * @param moves   liste des mouvements à effectuer
+ * @param nbMoves nombre de mouvement à effectuer
+ * @param player  joueur en train de jouer
+ */
+int move_all(SGameState * const, SMove const * const,
+		cuint, Dice[], cuint, const Player);
 
+/**
+ * @fn void move(SGameState * const state, SMove const move, const Player player)
+ * @brief Déplace un pion
+ * @param state  état du jeu
+ * @param move   mouvement à effectuer
+ * @param player joueur en train de jouer
+ * @pre le mouvement est valide
+ */
 void move(SGameState * const, SMove const, const Player);
-void move_from_out(SGameState * const, SMove const, const Player);
+/**
+ * @fn void move_from_out(SGameState* const state, SMove const move, const Player player)
+ * @brief Déplace un pion de la zone "out" à une case
+ * @param state  état du jeu
+ * @param move   mouvement à effectuer
+ * @param player joueur en train de jouer
+ * @pre le mouvement est valide
+ */
+void move_from_out(SGameState * const state, SMove const, const Player);
+/**
+ * @fn void move_to_end(SGameState* const state, SMove const move, const Player player)
+ * @brief déplace un pion dans la zone de fin
+ * @param state  état du jeu
+ * @param move   mouvement à effectuer
+ * @param player joueur en train de jouer
+ * @pre le mouvement est valide
+ */
 void move_to_end(SGameState * const, SMove const, const Player);
+/**
+ * @fn void move_in_board(SGameState* const state, SMove const move, const Player player)
+ * @brief déplace un pion d'une cas à une autre
+ * @param state  état du jeu
+ * @param move   mouvement à effectuer
+ * @param player joueur en train de jouer
+ * @pre le mouvement est valide
+ */
 void move_in_board(SGameState * const, SMove const, const Player);
+
+/**
+ * @fn void put_on(Square board[24], uint out[2], cuint dest, const Player player)
+ * @brief ajoute un pion dans la case
+ * @param board  plateau de jeu
+ * @param out    zone "out" pour mettre les pions adverse si ils sont mangé
+ * @param dest   case dans laquelle il faut mettre le pion
+ * @param player joueur en train de jouer
+ * @pre le mouvement est valide
+ *
+ */
+void put_on(Square[24], uint[2], cuint, const Player);
+/**
+ * @fn void take_from(Square[24] board, cuint source)
+ * @brief enleve un pion d'une case
+ * @param board  plateau de jeu
+ * @param source case ou il faut enlever le pion
+ * @pre le mouvement est valide
+ */
+void take_from(Square[24], cuint);
 
 #endif
--- interface/main.c
+++ interface/main.c
@@ -9,63 +9,86 @@
 
 #include"backgammon.h"
 #include"config.h"
+#include"referee.h"
+#include"init.h"
 
+//TODO : faire les test et merge la branche
+//TODO : appliquer clang-format
 
-typedef struct{
-	pfInitLibrary	initLibrary;
-	pfStartMatch	startMatch;
-	pfStartGame		startGame;
-	pfEndGame		endGame;
-	pfEndMatch		endMatch;
-	pfDoubleStack	doubleStack;
-	pfTakeDouble	takeDouble;
-	pfPlayTurn		playTurn;
-} Functions;
-
-void init_func(void* handle, Functions* func){
-	// TODO : faire le test si les fonctions ont bien été chargé
-	func->initLibrary = (pfInitLibrary)dlsym(handle, "InitLibrary");
-	func->startMatch  = (pfStartMatch)dlsym(handle, "StartMatch");
-	func->startGame   = (pfStartGame)dlsym(handle, "StartGame");
-	func->endGame	  = (pfEndGame)dlsym(handle, "EndGame");
-	func->endMatch 	  = (pfEndMatch)dlsym(handle, "EndMatch");
-	func->doubleStack = (pfDoubleStack)dlsym(handle, "DoubleStack");
-	func->takeDouble  = (pfTakeDouble)dlsym(handle, "TakeDouble");
-	func->playTurn    = (pfPlayTurn)dlsym(handle, "PlayTurn");
-}
-
-void init_lib(const char* lib_path, void** handle, Functions* func){
-	*handle = dlopen(lib_path, RTLD_NOW);
-	if(handle==NULL){
-		perror("Unable to open the library");
-		exit(EXIT_FAILURE);
-	}
-	dlerror();
-	init_func(*handle, func);
+void err(String str){
+	fprintf(stderr, "%s : %s\n", str, dlerror());
+	exit(EXIT_FAILURE);
 }
 
 int main(){
-	// --- Initialisation des bibliothèques
+	// --- Initialisation des bibliothèques {{{
 	void* white_handle = NULL;
 	Functions white_funcs;
 	char* white_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
 	sprintf(white_path, "%s/libstrategy.so", LIB_PATH);
-	init_lib(white_path, &white_handle, &white_funcs);
-	char white_name[50];
-	white_funcs.initLibrary(white_name);
-	printf("White I.A. : %s\n", white_name);
+	init_lib(white_path, &white_handle, &white_funcs, err);
 
 	void* black_handle = NULL;
 	Functions black_funcs;
 	char* black_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
 	sprintf(black_path, "%s/libstrategy.so", LIB_PATH);
-	init_lib(black_path, &black_handle, &black_funcs);
-	char black_name[50];
-	black_funcs.initLibrary(black_name);
-	printf("Black I.A. : %s\n", black_name);
+	init_lib(black_path, &black_handle, &black_funcs, err);
+	// }}}
 
-	// --- Fermeture des bibliothèques
+	// --- Initialisation du jeux {{{
+	SGameState state;
+	init_state(&state);//TODO
+
+	char black_name[50];
+	char white_name[50];
+	black_funcs.initLibrary(black_name);
+	white_funcs.initLibrary(white_name);
+	printf("Black I.A. : %s\n", black_name);
+	printf("White I.A. : %s\n", white_name);
+
+	cuint target_score = 1;
+	black_funcs.startMatch(target_score);
+	white_funcs.startMatch(target_score);
+	bool finished = false;
+	// }}}
+
+	// --- Boucle principale
+	while(!finished){
+		black_funcs.startGame(BLACK);
+		white_funcs.startGame(WHITE);
+		bool end_of_round = false;
+		uint score = 1;//TODO : Voir avec stake dans SGameState
+		Player winner = NOBODY;
+		while(!end_of_round){
+			//TODO : faire les copies de state
+			if( white_funcs.doubleStack(&state) ){
+				if( !black_funcs.takeDouble(&state) ){
+					end_of_round = true;
+					winner = WHITE;
+				} else {
+					score*=2;
+				}
+			} else if( black_funcs.doubleStack(&state) ) {
+				if( !white_funcs.takeDouble(&state) ){
+					end_of_round = true;
+					winner = BLACK;
+				} else {
+					score *= 2;
+				}
+			}
+			//TODO : reste
+		}
+		white_funcs.endGame();
+		black_funcs.endGame();
+	}
+
+	black_funcs.endMatch();
+	white_funcs.endMatch();
+
+
+	// --- Fermeture des bibliothèques {{{
 	dlclose(white_handle);
 	dlclose(black_handle);
+	// }}}
 	return EXIT_SUCCESS;
 }
