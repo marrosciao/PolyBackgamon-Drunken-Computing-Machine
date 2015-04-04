48fdbd8bcc2ae905c93df89d7eb3d2e2e2be0e39
n3f4s <tiv.crb@gmail.com>
2015-04-04 15:17:35 +0200

    ajout commentaire CMakeLists && debut debug main
    main non fonctionnel

--- interface/main.c
+++ interface/main.c
@@ -3,16 +3,16 @@
 #include<dlfcn.h>
 #include<string.h>
 #include<stdlib.h>
-#include<math.h>
-#include<time.h>
 
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
 
 #include"backgammon.h"
 #include"config.h"
-#include"referee.h"
 #include"init.h"
+#include"game.h"
+
+#define enum_to_string(val) #val
 
 //TODO : faire les test et merge la branche
 //TODO : appliquer clang-format
@@ -24,21 +24,6 @@ void err(String str){
 	exit(EXIT_FAILURE);
 }
 
-void roll_dice(unsigned char dice[2]){
-	srand(time(NULL));
-	dice[1] = (unsigned char)( (rand()%5)+1 );
-	dice[2] = (unsigned char)( (rand()%5)+1 );
-}
-void copy_state(SGameState state, SGameState* copy){
-	copy = malloc(sizeof(SGameState));
-	memcpy((void*)state.board, (void*)copy->board, 24);
-	memcpy((void*)state.out,   (void*)copy->out,   2);
-	memcpy((void*)state.bar,   (void*)copy->bar,   2);
-	copy->whiteScore = state.whiteScore;
-	copy->blackScore = state.blackScore;
-	copy->turn = state.turn;
-	copy->stake = state.stake;
-}
 Player choose_start_player()
 {
 	unsigned char dice[2];
@@ -51,34 +36,38 @@ Player choose_start_player()
 		return choose_start_player();
 }
 
-
 int main(){
+    IA players[2];
+    /*void* handle = dlopen("strategy/libstrategy.so", RTLD_NOW);*/
+    /*printf("%s\n", dlerror());*/
+    /*fflush(stdout);*/
+    /*pfInitLibrary fn = (pfInitLibrary)dlsym(handle, "InitLibrary");*/
+    /*printf("%s\n", dlerror());*/
+    /*fflush(stdout);*/
+    /*char name[50];*/
+    /*fn(name);*/
+    /*printf("%s\n", name);*/
 	//TODO : faire la gestion des paramêtres en ligne de commande
 	// --- Initialisation des bibliothèques {{{
-	void* white_handle = NULL;
-	Functions white_funcs;
-	char* white_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
-	sprintf(white_path, "%s/libstrategy.so", LIB_PATH);
-	init_lib(white_path, &white_handle, &white_funcs, err);
-
-	void* black_handle = NULL;
-	Functions black_funcs;
-	char* black_path = calloc(strlen(LIB_PATH)+strlen("/libstrategy.so")+1, sizeof(char));
-	sprintf(black_path, "%s/libstrategy.so", LIB_PATH);
-	init_lib(black_path, &black_handle, &black_funcs, err);
+    for(unsigned int i=0; i<2; ++i)
+    {
+        players[i].lib_handle = NULL;
+        players[i].lib_path = calloc(strlen("../strategy/libstrategy.so")+1, sizeof(char));
+        sprintf( (players[i].lib_path) , "../strategy/libstrategy.so");
+        players[i].func = malloc( sizeof(Functions) );
+        init_lib( (players[i].lib_path) , &(players[i].lib_handle), players[i].func, err);
+        //players[i].func.initLibrary( (players[i].name) );
+        printf("%p\n", players[i].func->endMatch);
+        players[i].func->endMatch();
+        printf("%s I.A. : %s\n", enum_to_string(i),players[i].name );
+    }
 	// }}}
-
+    /*
 	// --- Initialisation du jeux {{{
-	Player player = choose_start_player();
+	Player current = choose_start_player();
 	SGameState state;
 	init_state(&state);
 
-	char black_name[50];
-	char white_name[50];
-	black_funcs.initLibrary(black_name);
-	white_funcs.initLibrary(white_name);
-	printf("Black I.A. : %s\n", black_name);
-	printf("White I.A. : %s\n", white_name);
 
 	cuint target_score = 1;
 	black_funcs.startMatch(target_score);
@@ -105,60 +94,8 @@ int main(){
 		int black_tries = 3;
 		while(!end_of_round)
 		{
-			//TODO : faire les copies de state
-			//TODO : roll dice
-			roll_dice(dices);
-			SGameState* state_copy = NULL;
-			copy_state(state, state_copy);
-			if( white_funcs.doubleStack(state_copy) )
-			{
-				if( !black_funcs.takeDouble(state_copy) )
-				{
-					end_of_round = true;
-					winner = WHITE;
-				}
-				else
-				{
-					state.stake*=2;
-				}
-			}
-			else if( black_funcs.doubleStack(state_copy) )
-			{
-				if( !white_funcs.takeDouble(state_copy) )
-				{
-					end_of_round = true;
-					winner = BLACK;
-				}
-				else
-				{
-					state.stake *= 2;
-				}
-			}
-			if(!end_of_round)
-			{
-				//TODO : reste
-				if(player == WHITE)
-				{
-					white_funcs.playTurn(state_copy, dices, white_move, &white_nb_move, white_tries);
-					white_tries -= move_all(&state, white_move, white_nb_move, dices, 2, WHITE);
-				}
-				else
-				{
-					black_funcs.playTurn(state_copy, dices, black_move, &black_nb_move, black_tries);
-					black_tries -= move_all(&state, black_move, black_nb_move, dices, 2, BLACK);
-				}
-				if(black_tries<0)
-				{
-					winner = WHITE;
-					end_of_round = true;
-				}
-				else if(white_tries<0)
-				{
-					winner = BLACK;
-					end_of_round = true;
-				}
-				player = 1-player;
-			}
+            end_of_round = gamePlayTurn(&state, players, current, &lastStaker, &winner);
+            current = 1-current;
 		}
 		white_funcs.endGame();
 		black_funcs.endGame();
@@ -175,14 +112,15 @@ int main(){
 	}
 
 	black_funcs.endMatch();
-	white_funcs.endMatch();
+	white_funcs.endMatch();*/
 
 
 	// --- Fermeture des bibliothèques {{{
-	dlclose(white_handle);
-	dlclose(black_handle);
-	free(white_path);
-	free(black_path);
+    /*for(unsigned int i=0; i<2; ++i)*/
+    /*{*/
+        /*dlclose(players[i].lib_handle);*/
+        /*free(players[i].lib_path);*/
+    /*}*/
 	// }}}
 	return EXIT_SUCCESS;
 }
--- interface/src/init.c
+++ interface/src/init.c
@@ -2,74 +2,78 @@
 
 #include<dlfcn.h>
 #include<stdlib.h>
+#include<assert.h>
+#include<stdio.h>
 
 void init_func(void* handle, Functions* func, err_func err)
 {
-	//A cause de problème de conversion void* <-> pointeur de fonction,
-	//les appels à dlsym causent des warnings (avec l'option -pedantic).
-	//Ils sont dues à un défaut de conception de dlsym
-	func->initLibrary = (pfInitLibrary)dlsym(handle, "InitLibrary");
-	test(dlerror(), err);
-	func->startMatch  = (pfStartMatch)dlsym(handle, "StartMatch");
-	test(dlerror(), err);
-	func->startGame   = (pfStartGame)dlsym(handle, "StartGame");
-	test(dlerror(), err);
-	func->endGame	  = (pfEndGame)dlsym(handle, "EndGame");
-	test(dlerror(), err);
-	func->endMatch 	  = (pfEndMatch)dlsym(handle, "EndMatch");
-	test(dlerror(), err);
-	func->doubleStack = (pfDoubleStack)dlsym(handle, "DoubleStack");
-	test(dlerror(), err);
-	func->takeDouble  = (pfTakeDouble)dlsym(handle, "TakeDouble");
-	test(dlerror(), err);
-	func->playTurn    = (pfPlayTurn)dlsym(handle, "PlayTurn");
-	test(dlerror(), err);
+    //A cause de problème de conversion void* <-> pointeur de fonction,
+    //les appels à dlsym causent des warnings (avec l'option -pedantic).
+    //Ils sont dues à un défaut de conception de dlsym
+    func->initLibrary = (pfInitLibrary)dlsym(handle, "InitLibrary");
+    test(!dlerror() && func->initLibrary, err);
+    func->startMatch  = (pfStartMatch)dlsym(handle, "StartMatch");
+    test(!dlerror() && func->startMatch, err);
+    func->startGame   = (pfStartGame)dlsym(handle, "StartGame");
+    test(!dlerror() && func->startGame, err);
+    func->endGame      = (pfEndGame)dlsym(handle, "EndGame");
+    test(!dlerror() && func->endGame, err);
+    func->endMatch       = (pfEndMatch)dlsym(handle, "EndMatch");
+    test(!dlerror() && func->endMatch, err);
+    func->doubleStack = (pfDoubleStack)dlsym(handle, "DoubleStack");
+    test(!dlerror() && func->doubleStack, err);
+    func->takeDouble  = (pfTakeDouble)dlsym(handle, "TakeDouble");
+    test(!dlerror() && func->takeDouble, err);
+    func->playTurn    = (pfPlayTurn)dlsym(handle, "PlayTurn");
+    test(!dlerror() && func->playTurn, err);
 }
 
 void init_lib(const char* lib_path, void** handle, Functions* func, err_func err)
 {
-	*handle = dlopen(lib_path, RTLD_NOW);
-	test(*handle == NULL, err);
-	dlerror();
-	init_func(*handle, func, err);
+    printf("path : %s\n", lib_path);
+    *handle = dlopen(lib_path, RTLD_NOW);
+    test(*handle == NULL, err);
+    dlerror();
+    init_func(*handle, func, err);
+    assert(func->initLibrary);
 }
 
 void init_state(SGameState* state)
 {
-	for(unsigned int i=0; i<24; ++i)
-	{
-		if(i==12 || i==13 || i==6 || i==19)
-		{
-			state->board[i].nbDames = 5;
-			if(i==12 || 19) 		state->board[i].owner = WHITE;
-			else if(i==13 || i==6) 	state->board[i].owner = BLACK;
-		}
-		else if(i==8 || i==17)
-		{
-			state->board[i].nbDames = 3;
-			if(i==8) state->board[i].owner = BLACK;
-			else 	 state->board[i].owner = WHITE;
-		}
-		else if(i==1 || i==24)
-		{
-			state->board[i].nbDames = 2;
-			if(i==1) state->board[i].owner = WHITE;
-			else 	 state->board[i].owner = BLACK;
-		}
-		else
-		{
-			state->board[i].owner = NOBODY;
-			state->board[i].nbDames = 0;
-		}
-	}
-	for(unsigned int i=0; i<2; ++i)
-	{
-		state->bar[i] = 0;
-		state->out[i] = 0;
-	}
-	state->stake = 1;
-	state->turn = 0;
-	state->whiteScore = 0;
-	state->blackScore = 0;
+    for(unsigned int i=0; i<24; ++i)
+    {
+        if(i==12 || i==13 || i==6 || i==19)
+        {
+            state->board[i].nbDames = 5;
+            if(i==12 || 19)         state->board[i].owner = WHITE;
+            else if(i==13 || i==6)     state->board[i].owner = BLACK;
+        }
+        else if(i==8 || i==17)
+        {
+            state->board[i].nbDames = 3;
+            if(i==8) state->board[i].owner = BLACK;
+            else      state->board[i].owner = WHITE;
+        }
+        else if(i==1 || i==24)
+        {
+            state->board[i].nbDames = 2;
+            if(i==1) state->board[i].owner = WHITE;
+            else      state->board[i].owner = BLACK;
+        }
+        else
+        {
+            state->board[i].owner = NOBODY;
+            state->board[i].nbDames = 0;
+        }
+    }
+    for(unsigned int i=0; i<2; ++i)
+    {
+        state->bar[i] = 0;
+        state->out[i] = 0;
+    }
+    state->stake = 1;
+    state->turn = 0;
+    state->whiteScore = 0;
+    state->blackScore = 0;
 }
 
