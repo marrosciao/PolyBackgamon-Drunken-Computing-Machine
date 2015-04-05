dfb202121ac19bc17faf58e4716b8211a7f35a34
n3f4s <tiv.crb@gmail.com>
2015-04-05 18:07:27 +0200

    debug de l'arbitre et chargment lib

--- interface/main.c
+++ interface/main.c
@@ -3,6 +3,8 @@
 #include<dlfcn.h>
 #include<string.h>
 #include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
@@ -11,6 +13,7 @@
 #include"config.h"
 #include"init.h"
 #include"game.h"
+#include"logger.h"
 
 #define STRINGIFY_HELPER( str ) #str
 #define STRINGIFY( str ) STRINGIFY_HELPER( str )
@@ -21,97 +24,99 @@
 //TODO : gestion erreur
 
 void err(String str){
-	fprintf(stderr, "%s -> %s\n",str, dlerror());
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "%s -> %s\n",str, dlerror());
+    exit(EXIT_FAILURE);
 }
 
-Player choose_start_player()
+Player choose_start_player(unsigned int i)
 {
-	unsigned char dice[2];
-	roll_dice(dice);
-	if( dice[BLACK] > dice[WHITE])
-		return BLACK;
-	else if( dice[WHITE] > dice[BLACK] )
-		return WHITE;
-	else
-		return choose_start_player();
+    unsigned char dice[2];
+    roll_dice(dice);
+    Player tmp = BLACK;
+    if( dice[BLACK] > dice[WHITE])
+        tmp = BLACK;
+    else if( dice[WHITE] > dice[BLACK] )
+        tmp = WHITE;
+    else if( i<10 )
+        tmp = choose_start_player(++i);
+    return tmp;
 }
 
 int main(){
-    const char* enumToStr[] = {"NOBODY", "BLACK", "WHITE"};
+    init_logger();
+    const char* const enumToStr[] = {"NOBODY", "BLACK", "WHITE"};
     IA players[2];
-	//TODO : faire la gestion des paramêtres en ligne de commande
-	// --- Initialisation des bibliothèques {{{
+    //TODO : faire la gestion des paramêtres en ligne de commande
+    // --- Initialisation des bibliothèques 
     for(unsigned int i=0; i<2; ++i)
     {
-        players[i].lib_path = calloc(strlen(LIB_PATH) + strlen("/strategy/libstrategy.so")+1, sizeof(char));
+        players[i].lib_path = (char*)calloc(strlen(LIB_PATH) + strlen("/strategy/libstrategy.so")+1, sizeof(char));
         sprintf( players[i].lib_path , "%s/strategy/libstrategy.so", LIB_PATH);
-        players[i].func = malloc( sizeof(Functions) );
+        players[i].func = (Functions*)malloc( sizeof(Functions) );
         players[i].lib_handle = NULL;
         init_lib( players[i].lib_path , &(players[i].lib_handle), players[i].func, err);
         players[i].func->initLibrary( (players[i].name) );
         printf("%s I.A. : %s\n", enumToStr[i+1],players[i].name );
     }
-	// }}}
-    /*
-	// --- Initialisation du jeux {{{
-	Player current = choose_start_player();
-	SGameState state;
-	init_state(&state);
+    
+    // --- Initialisation du jeux 
+    SGameState state;
+    init_state(&state);
 
+    const unsigned int target_score = 1;
+    players[WHITE].func->startMatch(target_score);
+    players[BLACK].func->startMatch(target_score);
+    bool finished = false;
+    const unsigned int maxScore = 3;
+    unsigned int turn_num = 1;
+    srand(time(NULL));
 
-	cuint target_score = 1;
-	black_funcs.startMatch(target_score);
-	white_funcs.startMatch(target_score);
-	bool finished = false;
-	const unsigned int maxScore = 3;
-	// }}}
-
-	// --- Boucle principale
-	while(!finished)
-	{
-		//TODO :séparer en fonction
-		black_funcs.startGame(BLACK);
-		white_funcs.startGame(WHITE);
-		bool end_of_round = false;
-		state.stake = 1;
-		Player winner = NOBODY;
-		unsigned char dices[2];
-		SMove white_move[4];
-		unsigned int white_nb_move = 0;
-		int white_tries = 3;
-		SMove black_move[4];
-		unsigned int black_nb_move = 0;
-		int black_tries = 3;
-		while(!end_of_round)
-		{
+    // --- Boucle principale
+    while(!finished)
+    {
+        //TODO : faire des affichages pour voir si les fonctions sont bien lancé
+        //TODO : faire un logger basic
+        printf("Début de la manche %d\n", turn_num);
+        Player current = choose_start_player(0);
+        printf("\t%s commence\n", enumToStr[current+1]);
+        for(unsigned int i=0; i<2; ++i){
+            players[i].func->startGame((Player)i);
+            players[i].tries = 3;
+        }
+        bool end_of_round = false;
+        state.stake = 1;
+        Player winner = NOBODY;
+        Player lastStaker = NOBODY;
+        unsigned int round_num = 1;
+        while(!end_of_round)
+        {
+            printf("\tDébut du tour %d\n\t\tJoueur : %s\n", round_num, enumToStr[current+1]);
             end_of_round = gamePlayTurn(&state, players, current, &lastStaker, &winner);
-            current = 1-current;
-		}
-		white_funcs.endGame();
-		black_funcs.endGame();
-		if(winner==WHITE)
-		{
-			state.whiteScore+=state.stake;
-			finished = state.whiteScore==maxScore;
-		}
-		else
-		{
-			state.blackScore+=state.stake;
-			finished = state.blackScore==maxScore;
-		}
-	}
+            printf("\tfin du tour %d\n", round_num);
+            current = (Player)(1-current);
+        }
+        for(unsigned int i=0; i<2; ++i) players[i].func->endGame();
+        if(winner==WHITE)
+        {
+            state.whiteScore+=state.stake;
+            finished = state.whiteScore==maxScore;
+        }
+        else
+        {
+            state.blackScore+=state.stake;
+            finished = state.blackScore==maxScore;
+        }
+        printf("fin de la manche %d\n", turn_num);
+        printf("gagnant : %s\n", enumToStr[winner+1]);
+        ++turn_num;
+    }
+    for(unsigned int i=0; i<2; ++i) players[i].func->endMatch();
 
-	black_funcs.endMatch();
-	white_funcs.endMatch();*/
-
-
-	// --- Fermeture des bibliothèques {{{
-    /*for(unsigned int i=0; i<2; ++i)*/
-    /*{*/
-        /*dlclose(players[i].lib_handle);*/
-        /*free(players[i].lib_path);*/
-    /*}*/
-	// }}}
-	return EXIT_SUCCESS;
+    // --- Fermeture des bibliothèques 
+    for(unsigned int i=0; i<2; ++i)
+    {
+        dlclose(players[i].lib_handle);
+        free(players[i].lib_path);
+    }
+    return EXIT_SUCCESS;
 }
--- interface/src/game.c
+++ interface/src/game.c
@@ -5,17 +5,21 @@
 #include<math.h>
 #include<time.h>
 #include<string.h>
+#include<stdio.h>
 
 #include"referee.h"
 
+static Dice rand_dice(){
+    return (Dice)(rand()%6)+1;
+}
+
 void roll_dice(unsigned char dice[2]){
-    srand(time(NULL));
-    dice[1] = (unsigned char)( (rand()%5)+1 );
-    dice[2] = (unsigned char)( (rand()%5)+1 );
+    dice[0] = rand_dice();
+    dice[1] = rand_dice();
 }
 
 void copy_state(SGameState state, SGameState* copy){
-    copy = malloc(sizeof(SGameState));
+    copy = (SGameState*)malloc(sizeof(SGameState));
     memcpy((void*)state.board, (void*)copy->board, 24);
     memcpy((void*)state.out,   (void*)copy->out,   2);
     memcpy((void*)state.bar,   (void*)copy->bar,   2);
@@ -29,20 +33,26 @@ int gamePlayTurn(SGameState* state, IA player[2], Player current, Player* lastSt
 {
     unsigned char dices[2];
     roll_dice(dices);
+    printf("\t\trésultat des dés : %d, %d\n", dices[0], dices[1]);
     SGameState* state_copy = NULL;
     copy_state(*state, state_copy);
     bool end_of_round = false;
     *winner = NOBODY;
+    const char* const enumToStr[] = {"NOBODY", "BLACK", "WHITE"};
     if( *lastStaker!=current && player[current].func->doubleStack(state_copy) )
     {
+        printf("\t\t %s double la mise\n", enumToStr[current+1]);
         if( !player[1-current].func->takeDouble(state_copy) )
         {
+            printf("\t\t %s ne suit pas\n", enumToStr[(1-current)+1]);
             end_of_round = true;
             *winner = current;
         }
         else
         {
+            printf("\t\t %s suit\n", enumToStr[(1-current)+1]);
             state->stake *= 2;
+            *lastStaker = current;
         }
     }
     if( !end_of_round )
@@ -54,6 +64,9 @@ int gamePlayTurn(SGameState* state, IA player[2], Player current, Player* lastSt
                 &(player[current].nb_moves),
                 player[current].tries
         );
+        for(unsigned int i=0; i<player[current].nb_moves; ++i){
+            printf("%s : %d -> %d\n", enumToStr[current+1], player[current].moves[i].src_point, player[current].moves[i].dest_point);
+        }
         player[current].tries -= move_all(
                 state,
                 player[current].moves,
@@ -65,11 +78,9 @@ int gamePlayTurn(SGameState* state, IA player[2], Player current, Player* lastSt
     }
     if(player[current].tries<0)
     {
-        *winner = 1-current;
+        printf("\t\t%s a fait trop d'erreurs\n", enumToStr[current+1]);
+        *winner = (Player)(1-current);
         end_of_round = true;
     }
     return end_of_round;
 }
-
-
-
