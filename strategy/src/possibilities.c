8f0bd85bd95627c56e86517b4881015dc757548b
Mikaël Fourrier <mikael.fourrier@laposte.net>
2015-04-05 10:41:25 +0200

    Début d'implémentation du calcul des possibilités.

--- strategy/src/possibilities.c
+++ strategy/src/possibilities.c
@@ -4,10 +4,10 @@
 
 /** Pour toutes les fonctions ici, SGameState est normalisé. */
 
-bool is_move_possible(SGameState *game, Player player, uint location);
-bool is_valide_move(SGameState *game, Player player, uint from, uint len);
-void reverse_game(SGameState*);
-void reverse_move(SMove);
+bool        is_move_possible(SGameState *game, Player player, uint location);
+bool        is_valide_move(SGameState *game, Player player, uint from, uint len);
+SGameState  reverse_game(SGameState);
+AIListMoves reverse_moves(AIListMoves);
 
 bool is_valide_move(SGameState *game, Player player, uint from, uint len) {
     if (from > 25 || (from + len) >= 25) {
@@ -21,7 +21,7 @@ bool is_valide_move(SGameState *game, Player player, uint from, uint len) {
     if (from == 0) {
         return is_move_possible(game, player, from + len);
     } else if (from + len == 25) {
-        
+
     } else {
     }
 }
@@ -31,6 +31,44 @@ bool is_move_possible(SGameState *game, Player player, uint location) {
         game->board[location].owner == NOBODY) {
         return true;
     } else {
-            return game->board[location].nbDames < 2;
+        return game->board[location].nbDames < 2;
     }
+}
+
+ArrayList *retrieveEveryPossibility(SGameState game, Player player, const unsigned char dices[2]) {
+    if (player == BLACK) {
+        game = reverse_game(game);
+    }
+
+    ArrayList *list = list_new();
+
+    // TODO: implémenter !
+
+    if (player == BLACK) {
+        list_foreach(list, reverse_moves);
+    }
+    return list;
+}
+
+SGameState reverse_game(SGameState game) {
+    Square board[24];
+
+    for (size_t i = 0; i < 24; i++) {
+        board[i] = game.board[24 - i];
+    }
+
+    for (size_t i = 0; i < 24; i++) {
+        game.board[i] = board[i];
+    }
+
+    return game;
+}
+
+AIListMoves reverse_moves(AIListMoves moves) {
+    for (size_t i = 0; i < moves.nombre_mouvements; i++) {
+        moves.mouvement[i].src_point = 25 - moves.mouvement[i].src_point;
+        moves.mouvement[i].dest_point = 25 - moves.mouvement[i].dest_point;
+    }
+
+    return moves;
 }
