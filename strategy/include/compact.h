667a3ddd212d0af98a15b2cde7285c4b5c1d5847
Cobrand <cobrandw@gmail.com>
2015-04-09 22:40:52 +0200

    Attribute Packed sur les enums

--- strategy/include/compact.h
+++ strategy/include/compact.h
@@ -4,33 +4,24 @@
 #include "backgammon.h"
 #include <stdint.h>
 
-
-// C'est sale, mais ça fait l'équivalent d'un enum
-typedef uint8_t CompactPlayer ;
-static const CompactPlayer CNOBODY = -1 ;
-static const CompactPlayer CBLACK = 0 ;
-static const CompactPlayer CWHITE = 1 ;
-
-/*
-// On essaye de faire un enum ne prenant qu'un seule byte au lieu de 4 (pour l'alignement)
-// problème : ne marche pas
-typedef enum
+// On essaye de faire un enum ne prenant qu'un seule byte au lieu de 4
+typedef enum __attribute__ ((packed)) 
 {
 	CNOBODY = -1 ,
 	CBLACK = 0 ,
 	CWHITE = 1
-} CompactPlayer __attribute__ ((packed));
-*/
+} CompactPlayer;
+
 
 
 /**
  * @brief Définit le contenu d'une case de jeu.
  */
-typedef struct {
+typedef struct __attribute__ ((packed)) {
     CompactPlayer owner;           /*!<  Joueur contrôlant la case */
     uint8_t nbDames;   /*!<  Nombre de dames sur la case (non utilisé si
                                   `owner == NOBODY.`) */
-} CompactSquare;
+} CompactSquare ;
 
 
 /**
@@ -39,7 +30,7 @@ typedef struct {
  * Une struct contenant toutes les informations du jeu permet des tests plus
  * faciles.
  */
-typedef struct {
+typedef struct __attribute__ ((packed))  {
     CompactSquare board[24];           /*!< Les 24 cases du tableau, en partant du bord blanc. */
     uint8_t bar[2];
     uint8_t out[2];
@@ -50,7 +41,7 @@ typedef struct {
 } CompactGameState;
 
 // Structure de représentation d'un mouvement
-typedef struct
+typedef struct __attribute__ ((packed)) 
 {
     uint8_t src_point;
     uint8_t dest_point;
