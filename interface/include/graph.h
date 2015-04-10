2506dc20e08859b93eea1a1f2702cbae338a8a1d
Jean Labat <jeanlabat01@gmail.com>
2015-04-10 20:54:36 +0200

    MAJ doc human et graph

--- interface/include/graph.h
+++ interface/include/graph.h
@@ -11,19 +11,10 @@
  */
 SDL_Surface* initGraph();
 /**
- * @fn SGameState* copy_state(SGameState state)
- * @brief Copie un SGameState
- * @param       state état à copier
- * @return pointeurs vers la copie de l'état
- */
-
-void endGraph(); 
-/**
  * @fn void endGraph()
  * @brief Termine la SDL
  */
-
-void drawPiece(Player color,int posx,int posy,SDL_Surface* screen);
+void endGraph(); 
 /**
  * @fn void drawPiece(Player color,int posx,int posy,SDL_Surface* screen)
  * @brief Dessine un pion à la position donnée
@@ -32,30 +23,27 @@ void drawPiece(Player color,int posx,int posy,SDL_Surface* screen);
  * @param posy      coordonnée y
  * @param screen    surface sur laquelle on le dessine
  */
-
-void drawBackground(SDL_Surface* screen);
+void drawPiece(Player color,int posx,int posy,SDL_Surface* screen);
 /**
  * @fn void drawBackground(SDL_Surface* screen)
  * @brief Dessine le fond du jeu
  * @param screen    surface sur laquelle on le dessine
  */
-void drawDes(unsigned char dices[2], SDL_Surface* screen); 
+void drawBackground(SDL_Surface* screen);
 /**
  * @fn void drawDes(unsigned char dices[2], SDL_Surface* screen)
  * @brief dessineles des donnés
  * @param dices     Des à dessiner
  * @param screen    surface sur laquelle on les dessine
  */
-
-void animateDes(unsigned char dices[2], SDL_Surface* screen);
+void drawDes(unsigned char dices[2], SDL_Surface* screen); 
 /**
  * @fn void animateDes(unsigned char dices[2], SDL_Surface* screen)
  * @brief annime le roulement des des donnés
  * @param dices     Des à dessiner
  * @param screen    surface sur laquelle on les dessine
  */
-
-void printtext(int posx, int posy, char fontName[],int size, char message[],SDL_Color color, SDL_Surface* screen);
+void animateDes(unsigned char dices[2], SDL_Surface* screen);
 /**
  * @fn void printtext(int posx, int posy, char fontName[],int size, char message[],SDL_Color color, SDL_Surface* screen)
  * @brief affiche un texte donné
@@ -67,15 +55,13 @@ void printtext(int posx, int posy, char fontName[],int size, char message[],SDL_
  * @param color     couleur d'ecriture
  * @param screen    surface sur laquelle on les dessine
  */
-
-void drawBoard(SGameState* state, SDL_Surface* screen);
+void printtext(int posx, int posy, char fontName[],int size, char message[],SDL_Color color, SDL_Surface* screen);
 /**
  * @fn void drawBoard(SGameState* state, SDL_Surface* screen)
  * @brief Dessine le board (pions, videau, bar, out)
  * @param state     l'état courant à dessiner
  */
-
-bool hitbox(int x,int y,int i); 
+void drawBoard(SGameState* state, SDL_Surface* screen);
 /**
  * @fn bool hitbox(int x,int y,int i)
  * @brief verifie si les coordonnées x,y correspondent à une case du jeu i 
@@ -84,8 +70,7 @@ bool hitbox(int x,int y,int i);
  * @param i     indice de la case que l'on verifie  
  * @return true si on est dans la case, false sinon
  */
-
-int selectPion(SGameState* state, bool src, Player color);
+bool hitbox(int x,int y,int i); 
 /**
  * @fn int selectPion(SGameState* state, bool src, Player color)
  * @brief permet au joueur d'interagir avec le jeu en selectionnant les pions qu'il veut bouger ou en annulant ses coups ou en validant ses coups 
@@ -94,5 +79,7 @@ int selectPion(SGameState* state, bool src, Player color);
  * @param color     la couleur que joue le joueur, pour l'empecher de bouget des pions enemis  
  * @return un nombre entre 0 et 27 (de 0 à 25: cases du plateau; 26: validation des coups; 27: annulation des coups):
  */
+int selectPion(SGameState* state, bool src, Player color);
+
 
 #endif
--- interface/include/human.h
+++ interface/include/human.h
@@ -8,6 +8,13 @@
 void InitLibrary(char name[50]);
 void StartMatch(const unsigned int target_score);
 void StartGame(Player p);
+/**
+ * @fn Functions* StartScreen(SDL_Surface* s)
+ * @brief Permet de communiquer l'ecran sur lequel est affiché le jeu pour que le joueur humain
+aie accès à l'interface graphique, de plus, revoie des pointeurs vers toutes les fonctions qu'iutilisera l'IA, pour ne pas avoir à faire de libraire pour ce joueur "special"
+ * @param s     l'ecran que l'on communique au joueur
+ * @return renvoie un pointeur vers toutes autres fonctions de human, qui fonctionnent toutes exactement comme des fonctions d'une IA normale 
+ */
 Functions* StartScreen(SDL_Surface* s);
 void EndGame();
 void EndMatch();
